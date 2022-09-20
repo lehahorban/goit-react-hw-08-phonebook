f3185b951912f51094505f6f9e67377c19be9677
Oleksii Horban <lehagorban93@gmail.com>
2022-09-20 14:24:25 +0300

    commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
 import style from './Home.module.css';
 import contactImage from './img/pngwing.com.png';
+import image from './img/pngwing.com (3).png';
+import images from './img/pngwing.com (5).png';
 
 const Home = () => {
   return (
     <div className={style.home}>
       <h1 className={style.title}>Welcome to contacts app!</h1>
-      <img className={style.img} src={contactImage} alt="img" />
+      <div className={style.homeWrapp}>
+        {' '}
+        <img className={style.img} src={image} alt="img" />
+        <img className={style.img} src={contactImage} alt="img" />
+        <img className={style.img} src={images} alt="img" />
+      </div>
     </div>
   );
 };
